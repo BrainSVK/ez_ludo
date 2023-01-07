67f877d112b269ed1af0d2d24e95f1ab5cf779ea
erikv <erik.vrabec@gmail.com>
2023-01-07 22:02:24 +0100

    Initial commit

---
+++ clovece.h
@@ -0,0 +1,18 @@
+//
+// Created by erikv on 7. 1. 2023.
+//
+
+#ifndef EZ_LUDO_CLOVECE_H
+#define EZ_LUDO_CLOVECE_H
+
+void pridajHraca(int _uid,char* _meno);
+void vyhresliHraciuPlochu();
+void vykresliZacDomPlochu();
+void vykresliDomPlochu();
+int hodKockou();
+void vykresli(char* vykresli_);
+int getIdHraca(int index);
+void queue_remove_hrac(int uid);
+void vypisStavHraca(int index,char* vykresli_);
+
+#endif //EZ_LUDO_CLOVECE_H
